f8f9313c523b65f96ee05fc5043a27c61656d01a
Fed Finjap <ffinjap@yext.com>
2021-05-02 12:30:24 -0400

    initial commit

---
+++ text-extractor.py
@@ -0,0 +1,26 @@
+import sys
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+# Get url to extract text from via user input
+url = input('Enter the URL of the website you would like to extract text from: ')
+print('Extracting text from: ' + url)
+
+html = urlopen(url).read()
+soup = BeautifulSoup(html, features="html.parser")
+
+# kill all script and style elements
+for script in soup(["script", "style"]):
+    script.extract()    # rip it out
+
+# get text
+text = soup.get_text()
+
+# break into lines and remove leading and trailing space on each
+lines = (line.strip() for line in text.splitlines())
+# break multi-headlines into a line each
+chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+# drop blank lines
+text = '\n'.join(chunk for chunk in chunks if chunk)
+
+print(text)
